39aa06123c266f48d33688e514fa0e095ed08915
Calbabreaker <calbabreaker@gmail.com>
2022-02-19 14:46:14 +1030

    deploy

--- src/App.tsx
+++
@@ -1,5 +0,0 @@
-import { Board } from "./Board";
-
-export default function App() {
-    return <Board rows={15} cols={15} mines={30} />;
-}
--- src/vite-env.d.ts
+++
@@ -1,9 +0,0 @@
-/// <reference types="vite/client" />
-
-import "react";
-
-declare module "react" {
-    interface CSSProperties {
-        [key: `--${string}`]: string | number;
-    }
-}
